d2d52d1ae795308fb4c9e1725e51a93badd20812
ravi.nalluri1998@gmail.com <37662523+RaviTejaNalluri@users.noreply.github.com>
2024-09-05 15:43:07 +0530

    with eventListener

--- RPS/script.js
+++ RPS/script.js
@@ -1,73 +1,66 @@
-// function getComputerChoice(){
-//    let x=Math.floor(Math.random()*3);
-//    if(x==0){
-//     return "rock";
-//    }
-//    else if(x==1){
-//     return "paper";
-//    }
-//    else{
-//     return "scissor"
-//    }
-// }
+let hs=0;
+let cs=0;
+const buttons=document.querySelectorAll('input');
+function getComputerChoice(){
+   let x=Math.floor(Math.random()*3);
+   if(x==0){
+    return "Rock";
+   }
+   else if(x==1){
+    return "Paper";
+   }
+   else{
+    return "Scissors"
+   }
+}
+function disableButtons(){
+  buttons.forEach(button=>{
+    button.disabled=true;
+  })
+}
+function playRound(hc){
+    let cc=getComputerChoice();
+    let result=""
+    if(hc===cc){
+        result=("<br><br>It's a tie! You both chose "+hc);
+    }
+    else if(hc==="Rock" && cc==="Scissors" || hc==="Paper" && cc==="Rock" || hc==="Scissors" && cc==="Paper"){
+        hs++;
+        result=('<br><br>You Win! You selected '+hc+" which beats "+cc+"<br><br> PlayerScore:"+hs+"Computer Score:"+cs)
+        if(hs==5){
+          result+='<br><br>You won! Please refresh the page to play again'
+          disableButtons()
+        }
+    }
+    else{
+      cs++;
+      result=('<br><br>I Win! I selected '+cc+" which beats "+hc+"<br><br> PlayerScore:"+hs+"Computer Score:"+cs)
+      if(cs==5){
+        result+='<br><br>I won! You are dumb! Please refresh the page to become dumb again'
+        disableButtons()
+      }
+    }
+    document.getElementById("result").innerHTML=result
+    return
+}
+buttons.forEach(button=>{
+  button.addEventListener('click',function(){
+    playRound(button.value);
+  })
+})
 
-// function getHumanChoice(){
-//     while(true){
-//         const status=prompt("rock,paper,scissors").toLowerCase();
-//         if(status==="rock" || status==="paper" || status==="scissors"){
-//             return status;
-//         }
-//         else{
-//             console.log("Invalid Human choice");
-//         }
-//      }
-// }
-
-// function playGame(){
-//     console.log("Starting the game");
-//     let hs=0;
-//     let cs=0;
-//     function playRound(hc,cc){
-//         if(hc===cc){
-//             alert("It's a tie");
-//         }
-//         else if(hc==="rock" && cc==="scissors" || hc==="paper" && cc==="rock" || hc==="scissors" && cc==="paper"){
-//             alert("Humans win");
-//             hs++;
-//         }
-//         else{
-//             alert("Computer win");
-//             cs++;
-//         }
-//     }
-//     while(hs<5 && cs<5){
-//         const human=getHumanChoice();
-//         const computer=getComputerChoice();
-
-//         playRound(human,computer);
-//         console.log(`Current Score - Human: ${hs}, Computer: ${cs}`);
-//     }
-//     if(hs===5){
-//         console.log("You Win");
-//     }
-//     else{
-//         console.log("You lost! You are dumb");
-//     }
-// }
-
-// playGame();
 
 
 // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll("button");
+// const buttons = document.querySelectorAll("button");
 
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
-});
+// // we use the .forEach method to iterate through each button
+// buttons.forEach((button) => {
+//   // and for each one we add a 'click' listener
+//   button.addEventListener("click", () => {
+//     alert(button.id);
+//   });
+// });
 
 
 //THis is for DOM exercise
